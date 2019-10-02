9899e6fee990dad53f897ceb6974b1960928ddf4
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2019-10-02 17:18:55 +0300

    enabled login

--- src/app/user/service/client-profile/user-profile.service.ts
+++ src/app/user/service/client-profile/user-profile.service.ts
@@ -15,14 +15,14 @@ export class UserProfileService {
   requestUserDetails() {
     // TODO: Change Link to Match Login API Before Uploading
     return this.httpClient.get<UserResponse>(
-      'http://5d90a279b9f5430014c27280.mockapi.io/mock/login/1'
-      // UserConfig.userProfileAPI
+      // 'http://5d90a279b9f5430014c27280.mockapi.io/mock/login/1'
+      UserConfig.userProfileAPI
     );
   }
 
   requestUserLogout() {
     return this.httpClient.get(
-      'http://5d90a279b9f5430014c27280.mockapi.io/mock/login/1'
+      UserConfig.userProfileAPI
     );
   }
 }
