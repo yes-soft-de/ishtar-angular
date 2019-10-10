d7160226075335fd43f14d9a9a0d02d95364208c
Talal Danoun <tald7344@gmail.com>
2019-10-10 13:45:41 +0300

    delete bussiness-logic folder

--- src/app/user/bussiness-logic/painting-list-adapter/painting-list-adapter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PaintingListAdapter } from './painting-list-adapter';
-
-describe('PaintingListAdapter', () => {
-  it('should create an instance', () => {
-    expect(new PaintingListAdapter()).toBeTruthy();
-  });
-});
--- src/app/user/bussiness-logic/paintingDetailsAdapter/painting-details-adapter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PaintingDetailsAdapter } from './painting-details-adapter';
-
-describe('PaintingDetailsAdapter', () => {
-  it('should create an instance', () => {
-    expect(new PaintingDetailsAdapter()).toBeTruthy();
-  });
-});
