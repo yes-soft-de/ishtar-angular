ffa5053c6fe4bb5a2e3a04dd6ab26e7dee3a3df7
Talal Danoun <tald7344@gmail.com>
2019-10-22 22:25:31 +0300

    styling statue detail page for wide screen

--- src/app/user/ui/components/statues-detail/statues-detail.component.ts
+++ src/app/user/ui/components/statues-detail/statues-detail.component.ts
@@ -10,6 +10,26 @@ export class StatuesDetailComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById('defaultOpen').click();
   }
+
+  openTab(evt, cityName) {
+    let i;
+    let tabContent;
+    let tabLinks;
+    tabContent = document.getElementsByClassName('tab-content');
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = 'none';
+    }
+    tabLinks = document.getElementsByClassName('tab-links');
+    for (i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].className = tabLinks[i].className.replace(' active', '');
+    }
+    document.getElementById(cityName).style.display = 'block';
+    evt.currentTarget.className += ' active';
+  }
+
+
 
 }
