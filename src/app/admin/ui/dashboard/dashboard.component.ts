d982dcb72648910861320ce8c62c43504e98842d
Talal Danoun <tald7344@gmail.com>
2019-10-27 22:45:39 +0200

    add list statues page in admin section

--- src/app/admin/ui/dashboard/dashboard.component.ts
+++ src/app/admin/ui/dashboard/dashboard.component.ts
@@ -8,6 +8,9 @@ import {AuctionListResponse} from '../../entity/auction/auction-list-response';
 import {AuctionService} from '../../service/auction/auction.service';
 import {AuctionList} from '../../entity/auction/auction-list';
 import {Subscription} from 'rxjs';
+import {StatuesResponse} from '../../entity/statue/statues.response';
+import {StatueService} from '../../service/statue/statue.service';
+import {StatueInterface} from '../../entity/statue/statue.interface';
 
 
 @Component({
@@ -19,16 +22,21 @@ export class DashboardComponent implements OnInit, OnDestroy {
   artists: Artist[];
   paintings: Painting[];
   auctions: AuctionList[];
+  statues: StatueInterface[];
   latestArtistNumber = 5;
   latestPaintingNumber = 5;
+  latestStatueNumber = 5;
   latestAuctionNumber = 5;
   allArtistsObservable: Subscription;
   allPaintingsObservable: Subscription;
   allAuctionObservable: Subscription;
+  allStatuesObservable: Subscription;
 
   constructor(private artist: ArtistService,
               private photosListService: PhotosListService,
-              private auctionService: AuctionService) { }
+              private auctionService: AuctionService,
+              private statueService: StatueService) { }
+
   ngOnInit() {
     // Fetch All Artists Number
     this.allArtistsObservable = this.artist.getAllArtists().subscribe(
@@ -51,12 +59,21 @@ export class DashboardComponent implements OnInit, OnDestroy {
         }, error => {
           console.log(error);
         });
+    // Fetch All Statues
+    this.allStatuesObservable = this.statueService.getAllStatues().subscribe(
+        (data: StatuesResponse) => {
+          this.statues = data.Data;
+        }, error => {
+          console.log(error);
+        }
+    );
   }
 
   ngOnDestroy() {
     this.allArtistsObservable.unsubscribe();
     this.allPaintingsObservable.unsubscribe();
     this.allAuctionObservable.unsubscribe();
+    this.allStatuesObservable.unsubscribe();
   }
 
 }
--- src/app/admin/ui/statue/list-statue/list-statue.component.ts
+++ src/app/admin/ui/statue/list-statue/list-statue.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {StatueService} from '../../../service/statue/statue.service';
+import {StatuesResponse} from '../../../entity/statue/statues.response';
+import {StatueInterface} from '../../../entity/statue/statue.interface';
 
 @Component({
   selector: 'app-list-statue',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-statue.component.scss']
 })
 export class ListStatueComponent implements OnInit {
+  statues: StatueInterface[];
 
-  constructor() { }
+  constructor(private statueService: StatueService) { }
 
   ngOnInit() {
+    // Get All Statues
+    this.statueService.getAllStatues().subscribe(
+        (data: StatuesResponse) => {
+          this.statues = data.Data;
+          console.log(data);
+        }, error => {
+          console.log(error);
+        }
+    );
   }
+
 
 }
