ce47878818fe1fc99cc4bc8a696cc94922a7989e
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2019-11-19 22:28:17 +0200

    Update painting-manager.service.ts

--- src/app/user/painting/manager/painting-manager.service.ts
+++ src/app/user/painting/manager/painting-manager.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {PaintingRepositoryService} from '../repository/painting-repository.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaintingManagerService {
 
-  constructor() { }
+  constructor(private paintingRepository: PaintingRepositoryService) {
+  }
+
+  getPainting(paintingId: number) {
+    this.paintingRepository.getPainting(paintingId);
+  }
 }
