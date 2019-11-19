79395a35c88e9a909e0f83fb282b372bd953497e
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2019-11-19 22:34:08 +0200

    Update painting.service.ts

--- src/app/user/painting/service/painting.service.ts
+++ src/app/user/painting/service/painting.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {PaintingManagerService} from '../manager/painting-manager.service';
+import {PaintingDetails} from '../entity/painting-details';
+import {Observable, Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaintingService {
+  private serviceSubject: Subject<PaintingDetails>;
 
-  constructor() { }
+  constructor(private paintingManager: PaintingManagerService) {
+  }
+
+  getPainting(paintingId): Observable<PaintingDetails> {
+    this.paintingManager.getPainting(paintingId).subscribe(
+      paintingResponse => {
+        this.serviceSubject.next(paintingResponse.Data);
+      }
+    );
+
+    return this.serviceSubject.asObservable();
+  }
 }
