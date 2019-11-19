123e85cbe6f105949f4f7ff9ee377ef3bc6d02a9
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2019-11-19 22:12:12 +0200

    Update painting-repository.service.ts

--- src/app/user/painting/repository/painting-repository.service.ts
+++ src/app/user/painting/repository/painting-repository.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PaintingDetailsResponse } from '../response/painting-details-response';
+import { catchError } from 'rxjs/operators';
+import { EMPTY, Subject } from 'rxjs';
+import { PaintingDetails } from '../../entity/painting-details/painting-details';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaintingRepositoryService {
+  repoSubject: Subject<PaintingDetails>;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getPainting(paintingId: number) {
+    this.httpClient.get<PaintingDetailsResponse>(`http://dev-ishtar.96.lt/ishtar-backend/public/painting/${paintingId}`)
+    .pipe(catchError(err => {
+      console.log(`${err}`);
+      return EMPTY;
+    })).subscribe(
+      data => {
+        this.repoSubject.next(data.Data);
+      }
+    );
+    return this.repoSubject;
+  }
 }
