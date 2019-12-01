302e7713a31acbf5ed5e771d1eb77ceaa026b9b1
Talal Danoun <tald7344@gmail.com>
2019-12-01 19:04:06 +0200

    fix the bug in statues component

--- src/app/user/statue/service/statue-comment.service.ts
+++ src/app/user/statue/service/statue-comment.service.ts
@@ -1,9 +1,9 @@
 import {Injectable} from '@angular/core';
 import {CommentManagerService} from '../../shared/comment/manager/comment-manager.service';
 import {BaseCommentService} from '../../shared/comment/service/base-comment.service';
-import {PageTypeToNumberService} from '../../shared/helper/page-type-to-number.service';
 import {Observable, Subject} from 'rxjs';
 import {CommentObject} from '../../shared/comment/entity/comment-object';
+import {PageTypeToNumberService} from '../../shared/helper/page-type-to-number.service';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +18,7 @@ export class StatueCommentService extends BaseCommentService {
   }
 
   getStatueComment(statueId: number): Observable<CommentObject[]> {
-    const apiType = this.pageTypeToNumberService.convertPageTypeToNumber('statue');
+    const apiType = this.pageTypeToNumberService.convertPageTypeToNumber(PageTypeToNumberService.ENTITY_TYPE_STATUE);
     this.getComments(apiType, statueId).subscribe(
       commentResponse => {
         this.statueCommentSubject.next(commentResponse.Data);
