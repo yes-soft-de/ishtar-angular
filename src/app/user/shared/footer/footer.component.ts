6ae7b8db45c94f3a223aefb153879bc83b2143b9
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2020-05-30 13:42:24 +0300

    added whatsapp click event

--- src/app/user/shared/footer/footer.component.ts
+++ src/app/user/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {GoogleAnalyticsService} from 'ngx-google-analytics';
 
 @Component({
   selector: 'app-footer',
@@ -6,12 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  whatsAppLink = 'https://wa.me/491633733146?text=I\'m%20interested%20in%20Painting';
   footerHeight: number;
-  constructor() { }
+
+  constructor(private router: Router, private googleAnalyticsService: GoogleAnalyticsService) {
+  }
 
   ngOnInit() {
     // Get Bottom Footer height
     this.footerHeight = document.getElementById('custom-id-bottom-footer').offsetHeight;
   }
 
+  async navigateToWhatsApp() {
+    await this.googleAnalyticsService.event('CONTACT_REQUEST');
+    await this.router.navigate([this.whatsAppLink]);
+  }
 }
